a46519a265cb0571314752ebe469efc9ac6e7326
ccorrea <ccorrea@bidda.net>
2024-01-22 22:49:47 -0500

    first commit

---
+++ tween.js/tween.js
@@ -0,0 +1,10 @@
+/*
+ * * * MINIFIED VERSION * * *
+ * Skypack CDN - @tweenjs/tween.js@21.0.0
+ * Remove "min" from url for additional help & instructions.
+ */
+
+export * from '/-/@tweenjs/tween.js@v21.0.0-kP69roXIumCvXe6TgNO3/dist=es2020,mode=imports,min/optimized/@tweenjs/tweenjs.js';
+export {default} from '/-/@tweenjs/tween.js@v21.0.0-kP69roXIumCvXe6TgNO3/dist=es2020,mode=imports,min/optimized/@tweenjs/tweenjs.js';
+
+// Perf Optimization: Preload Deep Imports
